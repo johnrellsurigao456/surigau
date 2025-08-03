3e9dcc05adc7d735b0d1edb403462e899abb6f39
johnrellsurigao456 <johnrellsurigao456@gmail.com>
2025-08-03 14:24:03 +0800

    Update activity2.java
    2

--- src/activity1/activity2.java
+++ src/activity1/activity2.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package activity1;
 
-/**
- *
- * @author PC1
- */
+
 public class activity2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number == 0) {
+            System.out.println("Inputted number Zero");
+        } else if (number % 2 == 0) {
+            System.out.println("Inputted number is even");
+        } else {
+            System.out.println("Inputted number is odd");
+        }
+        sc.close();
+    }
     
 }
+
